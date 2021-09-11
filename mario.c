9a73a49d2184ffce82750854140afd5d0a3597e8
AnneC.Yates <annecreeyates@gmail.com>
2021-09-11 20:52:01 +0100

    Update mario.c
    Adds the code that  was submitted to CS50 course for the less comfortable option

--- mario.c
+++ mario.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    int height = 0;
+
+    //Asks the user to provide a number between 1 nad 8 inclusive
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height > 8 || height < 1);
+
+    for (int i = 0; i < height; i ++)
+    {
+        for (int k = 1; k < height - i; k++)
+        {
+            printf("*");
+
+            //k adds the left hand spaces in the pyramid
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+            //i and j loops pint our the right hand pyramid
+        }
+
+        printf("\n");
+    The above prints out the left handed puramid (less confortable option)
+
+    }
+
+
+    
